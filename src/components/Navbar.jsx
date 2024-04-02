f9462309b79f28256f9c9d2d9ecb66afd30f2e11
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-02 19:36:06 +0100

    implemented conditional rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,11 @@ import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+      : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <>
       <nav className='bg-indigo-700 border-b border-indigo-500'>
@@ -19,23 +24,13 @@ const Navbar = () => {
               </NavLink>
               <div className='md:ml-auto'>
                 <div className='flex space-x-2'>
-                  <NavLink
-                    to='/'
-                    className={({ isActive }) =>
-                      isActive
-                        ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                        : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                    }>
+                  <NavLink to='/' className={linkClass}>
                     Home
                   </NavLink>
-                  <NavLink
-                    to='/jobs'
-                    className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                  <NavLink to='/jobs' className={linkClass}>
                     Jobs
                   </NavLink>
-                  <NavLink
-                    to='/add-job'
-                    className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                  <NavLink to='/add-job' className={linkClass}>
                     Add Job
                   </NavLink>
                 </div>
