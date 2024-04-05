977949523a756fb9b072c06de8effdcadf8c8fe8
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-05 23:25:28 +0100

    single job page added

---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const JobPage = () => {
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/job/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, []);
+
+  return <div>JobPage</div>;
+};
+
+export default JobPage;
