75cbf85886d4f2260eb1f2d572000e38c88d1d3d
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-09 21:25:28 +0100

    added toast for adding and deleting jobs

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import ToastContainer from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../components/Navbar';
 
