f5cbf00c0ebd4366c4f7c619424e5985fdfea995
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-09 21:59:08 +0100

    added edit page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   //add new job
@@ -32,12 +33,29 @@ const App = () => {
     return;
   };
 
+  // update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path='/edit-job/:id'
+          element={<EditJobPage updateJobSubmit={updateJob}/>}
+          loader={jobLoader}
+        />
         <Route
           path='/jobs/:id'
           element={<JobPage deleteJob={deleteJob} />}
