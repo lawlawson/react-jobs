f0c75a36205b20063936a8aeccf3a6edada8e527
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-09 18:06:13 +0100

    deleted job function added

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,10 @@ const App = () => {
 
   // delete job
   const deleteJob = async (id) => {
-    console.log('delete', id);
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
   };
 
   const router = createBrowserRouter(
